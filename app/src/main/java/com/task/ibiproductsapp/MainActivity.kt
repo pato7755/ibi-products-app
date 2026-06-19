380afe8c46ec9b9df405fe6e2d0bcbebd732ffb2
Patrick Essiam <pat.essiam701@gmail.com>
2026-06-19 10:47:02 +0000

    fix app startup UI glitch where login screen is shown briefly before ProductScreen when user is already logged in

--- app/src/main/java/com/task/ibiproductsapp/MainActivity.kt
+++ app/src/main/java/com/task/ibiproductsapp/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.task.ibiproductsapp.ui.theme.IbiProductsAppTheme
 import com.task.ibiproductsapp.util.DataStoreHelper
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -18,15 +24,29 @@ class MainActivity : AppCompatActivity() {
     lateinit var dataStoreHelper: DataStoreHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        var isReady by mutableStateOf(false)
+        var isLoggedIn by mutableStateOf(false)
+
+        splashScreen.setKeepOnScreenCondition { !isReady }
+
+        lifecycleScope.launch {
+            isLoggedIn = dataStoreHelper.isLoggedIn().first()
+            isReady = true
+        }
+
         enableEdgeToEdge()
 
         setContent {
-            val isDarkMode by dataStoreHelper.isDarkMode().collectAsStateWithLifecycle(initialValue = false)
-            val isLoggedIn by dataStoreHelper.isLoggedIn().collectAsStateWithLifecycle(initialValue = false)
+            if (isReady) {
+                val isDarkMode by dataStoreHelper.isDarkMode()
+                    .collectAsStateWithLifecycle(initialValue = false)
 
-            IbiProductsAppTheme(darkTheme = isDarkMode) {
-                AppNavGraph(isLoggedIn = isLoggedIn)
+                IbiProductsAppTheme(darkTheme = isDarkMode) {
+                    AppNavGraph(isLoggedIn = isLoggedIn)
+                }
             }
         }
     }
