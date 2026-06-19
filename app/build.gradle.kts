24f145e3957e265a7f9e984b9e1b7200f63ad2c2
Patrick Essiam <pat.essiam701@gmail.com>
2026-06-19 10:25:05 +0000

    add dependencies for mockk, turbine and coroutines test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,6 +98,9 @@ dependencies {
     implementation(libs.androidx.splashscreen)
 
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.turbine)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
